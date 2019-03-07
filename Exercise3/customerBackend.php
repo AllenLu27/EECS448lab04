ca6b90028ab61d69fa88a8e572c8288615c7ca42
BoHui Lu <a500l206@ku.edu>
2019-03-07 13:41:30 -0600

    Backend format fixed.

--- Exercise3/customerBackend.php
+++ Exercise3/customerBackend.php
@@ -1,5 +1,9 @@
 <?php
-echo "<style>table, th, td {border: 1px solid black;}</style>";
+echo "<style>table{border: 1px solid black;}
+      th{padding-top: 12px; padding-bottom: 12px; text-align: left; background-color: #4CAF50; color: white;}
+      tr{background-color: #f2f2f2;}
+      td{border: 1px solid #ddd; padding: 8px;}
+      </style>";
 $name = $_POST["name"];
 $password = $_POST["password"];
 $shirt = $_POST["shirt"];
@@ -12,7 +16,7 @@ $totalforshirt=90*$shirt;
 $totalforshoes=200*$shoes;
 $totalforglasses=75*$glasses;
 $totalfort_shirt=50*$t_shirt;
-$rowspan="2";
+$rowspan=2;
 $total=$totalforshirt+$totalforshoes+$totalforglasses+$totalfort_shirt+$shipping;
 if($shipping==0)
 {
@@ -33,8 +37,11 @@ echo "<table><tr><th> </th><th>Quantity</th><th>Cost Per Item</th><th>Sub Total<
       <tr><td>Shoes</td><td>$shoes</td><td>$200</td><td>$$totalforshoes</td></tr>
       <tr><td>Glasses</td><td>$glasses</td><td>$75</td><td>$$totalforglasses</td></tr>
       <tr><td>T-Shirt</td><td>$t_shirt</td><td>$50</td><td>$$totalfort_shirt</td></tr>
-      <tr><td>Shipping</td><td>$shippingoption</td><td></td><td>$$shipping</td></tr>
-      <tr><td rowspan='3'>Total Cost</td><td>$$total</td></tr>
-      </table>";
+      <tr><td>Shipping</td><td colspan='2'>$shippingoption</td><td>$$shipping</td></tr>";
+      echo "<tr>";
+      echo "<td colspan='3'>Total Cost</td>";
+      echo "<td>$$total</td>";
+      echo "</tr>";
+      echo "</table>";
 
 ?>
